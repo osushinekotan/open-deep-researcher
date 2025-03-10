566a5380efbb1d45e2e75c71e3f8aeea98d62975
osushinekotan <spmt3710@gmail.com>
2025-03-10 14:08:27 +0900

    refactor: increase word limits for sections and subsections in configuration

--- open_deep_researcher/configuration.py
+++ open_deep_researcher/configuration.py
@@ -40,10 +40,10 @@ class Configuration:
     number_of_queries: int = 2  # Number of search queries to generate per iteration
     max_reflection: int = 2  # Maximum number of reflection + search iterations
 
-    max_section_words: int = 1000  # セクション（main body）の最大単語数
-    max_subsection_words: int = 500  # サブセクションの最大単語数
-    max_introduction_words: int = 500  # イントロダクションの最大単語数
-    max_conclusion_words: int = 500  # 結論の最大単語数
+    max_section_words: int = 2048  # セクション（main body）の最大単語数
+    max_subsection_words: int = 2048  # サブセクションの最大単語数
+    max_introduction_words: int = 2048  # イントロダクションの最大単語数
+    max_conclusion_words: int = 2048  # 結論の最大単語数
 
     enable_deep_research: bool = True
     deep_research_depth: int = 1
